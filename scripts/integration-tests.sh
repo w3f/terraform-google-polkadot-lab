641c236317302e1cdbbcd644597b693e576d8b94
Federico Gimenez <federico.gimenez@gmail.com>
2020-03-20 18:50:15 +0100

    setup credentials for integration tests

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+credentials=$(pwd)/credentials
+echo ${GOOGLE_APPLICATION_CREDENTIALS_CONTENT} > $credentials
+export GOOGLE_APPLICATION_CREDENTIALS=$credentials
+
 cd test && go test -v -timeout 30m
