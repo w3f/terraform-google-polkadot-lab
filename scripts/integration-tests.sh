2ff76c166f01688d11d1474505cca29ac28fc40f
Federico Gimenez <federico.gimenez@gmail.com>
2020-03-20 18:55:12 +0100

    added quotes

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 credentials=$(pwd)/credentials
-echo ${GOOGLE_APPLICATION_CREDENTIALS_CONTENT} > $credentials
+echo "${GOOGLE_APPLICATION_CREDENTIALS_CONTENT}" > $credentials
 export GOOGLE_APPLICATION_CREDENTIALS=$credentials
 
 cd test && go test -v -timeout 30m
